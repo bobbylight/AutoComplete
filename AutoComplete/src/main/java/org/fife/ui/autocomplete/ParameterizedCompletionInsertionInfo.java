1a9b0132300cbe8b757853b6478833410a8f3a40
matt-pan <35034195+matt-pan@users.noreply.github.com>
2026-08-13 15:24:40 +0200

    Update usage of DocumentRange for new package path, compatibility with RSyntaxTextArea 4.0.1

--- AutoComplete/src/main/java/org/fife/ui/autocomplete/ParameterizedCompletionInsertionInfo.java
+++ AutoComplete/src/main/java/org/fife/ui/autocomplete/ParameterizedCompletionInsertionInfo.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.text.Position;
 
-import org.fife.ui.rsyntaxtextarea.DocumentRange;
+import org.fife.ui.rtextarea.DocumentRange;
 
 
 /**
